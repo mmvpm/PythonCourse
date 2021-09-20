be4c4634569fce63b44466bfd44e021295a90445
IdeaSeeker <stroganov.n.36@gmail.com>
2021-09-20 22:30:03 +0300

    contract initial

---
+++ Contract/contract.py
@@ -0,0 +1,10 @@
+class ContractError(Excepetion):
+    """We use this error when someone breaks our contract."""
+
+
+#: Special value, that indicates that validation for this type is not required.
+Any = object()
+
+
+def contract(arg_types=None, return_type=None, raises=None):
+    ...  # TODO: implement
