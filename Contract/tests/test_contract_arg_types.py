d7b20421fb5bc7cc7ea74e51d6434197ead8204a
IdeaSeeker <stroganov.n.36@gmail.com>
2021-10-28 16:20:31 +0300

    fixed spaces

--- Contract/tests/test_contract_arg_types.py
+++ Contract/tests/test_contract_arg_types.py
@@ -4,82 +4,82 @@ from contract import *
 
 
 def test_arg_types_happy_path():
-    
+
     @contract(arg_types=(int, float))
     def add_two_numbers(first, second):
         return first + second
-    
+
     add_two_numbers(1, 2.0)
 
 
 def test_arg_types_failed():
-    
+
     @contract(arg_types=(int, float))
     def add_two_numbers(first, second):
         return first + second
-    
+
     try:
-        add_two_numbers(1, 2) # (int, int)
+        add_two_numbers(1, 2)  # (int, int)
     except ContractError:
-        pass # ok
+        pass  # ok
     except Exception:
         assert False
 
 
 def test_arg_types_any():
-    
+
     @contract(arg_types=(int, Any))
     def add_two_numbers(first, second):
         return first + second
-    
+
     add_two_numbers(1, 2.0)
 
 
 def test_arg_types_wrong_any():
-    
+
     @contract(arg_types=(Any, float))
     def add_two_numbers(first, second):
         return first + second
-    
+
     try:
-        add_two_numbers(1, 2) # (int, int)
+        add_two_numbers(1, 2)  # (int, int)
     except ContractError:
-        pass # ok
+        pass  # ok
     except Exception:
         pytest.fail()
 
 
 def test_arg_types_none():
-    
+
     @contract(arg_types=None)
     def add_two_numbers(first, second):
         return first + second
-    
+
     add_two_numbers(1, 2.0)
 
 
 @pytest.mark.timeout(10)
 def test_arg_types_random_numbers():
-    
+
     @contract(arg_types=(int, float))
     def add_two_numbers(first, second):
         return first + second
-    
+
     first_number = random.randint(0, 5)
     second_number = random.random()
     add_two_numbers(first_number, second_number)
 
 
 @pytest.mark.parametrize(
-    'first_number', 
+    'first_number',
     [1, 2, 3, 5, 8, 13]
 )
 @pytest.mark.parametrize(
-    'second_number', 
+    'second_number',
     [0, 1, 2, 3, 5, 8]
 )
 def test_arg_types_special_numbers(first_number, second_number):
-    
+
     @contract(arg_types=(int, int))
     def add_two_numbers(first, second):
         return first + second
@@ -88,7 +88,7 @@ def test_arg_types_special_numbers(first_number, second_number):
 
 
 def test_arg_types_dataset(dataset):
-    
+
     @contract(arg_types=(float, float))
     def add_two_numbers(first, second):
         return first + second
--- Contract/tests/test_contract_raises.py
+++ Contract/tests/test_contract_raises.py
@@ -2,51 +2,51 @@ from contract import *
 
 
 def test_raises_happy_path():
-    
+
     @contract(raises=(ValueError,))
     def add_two_numbers(first, second):
         return first + int(second)
-    
+
     add_two_numbers(1, 2.0)
 
 
 def test_raises_right_error():
-    
+
     @contract(raises=(ValueError,))
     def add_two_numbers(first, second):
         return first + int(second)
-    
+
     try:
         add_two_numbers(1, 'a')
     except ValueError:
-        pass # ok
+        pass  # ok
     except Exception:
         assert False
 
 
 def test_raises_wrong_error():
-    
+
     @contract(raises=(TypeError,))
     def add_two_numbers(first, second):
         return first + int(second)
-    
+
     try:
         add_two_numbers(1, 'a')
     except ContractError:
-        pass # ok
+        pass  # ok
     except Exception:
         assert False
 
 
 def test_raises_none():
-    
+
     @contract(raises=None)
     def add_two_numbers(first, second):
         return first + int(second)
-    
+
     try:
         add_two_numbers(1, 'a')
     except ValueError:
-        pass # ok
+        pass  # ok
     except Exception:
         assert False
